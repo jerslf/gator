86da4daf10eb9d1470f115cddaad9660c7f65a62
jerslf <jerslf@hotmail.co.uk>
2025-09-21 14:27:13 +0200

    Add posts functionality with database integration and user retrieval

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/jerslf/gator
 
 go 1.24.5
 
-require github.com/google/uuid v1.6.0
-
-require github.com/lib/pq v1.10.9
+require (
+	github.com/google/uuid v1.3.0
+	github.com/lib/pq v1.10.7
+)
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/jerslf/gator/internal/config"
 	"github.com/jerslf/gator/internal/database"
+
 	_ "github.com/lib/pq"
 )
 
@@ -37,6 +38,7 @@ func main() {
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+
 	cmds.register("register", handlerRegister)
 	cmds.register("login", handlerLogin)
 	cmds.register("reset", handlerReset)
@@ -47,6 +49,7 @@ func main() {
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
 	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
+	cmds.register("browse", middlewareLoggedIn(handlerBrowse))
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
